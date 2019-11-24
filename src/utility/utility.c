b46c5591c2ae4604fc59f029c60e8fe36635a84b
Jones Napoleon <jonesnapoleonautumn1@gmail.com>
2019-11-24 17:26:27 +0700

    little change

--- src/utility/utility.c
+++ src/utility/utility.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include "../utility/utility.h"
+#include "utility.h"
 
 void LineBr() {
   printf("--------------------------------------------------\n");
